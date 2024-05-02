fafb2d818c3090beec18cb7e65355cc178056874
william.quimbay <quimbayfeo@gmail.com>
2024-05-02 16:16:54 -0500

    resposive

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -4,12 +4,48 @@ document.addEventListener("DOMContentLoaded", (event) => {
   let mobile_btn = document.querySelector(".navbar__mobile-btn");
   let mobile_menu = document.querySelector(".menu-mobile");
 
-  mobile_btn.addEventListener("click", () => {
+  //funcion de mostrar y ocultar
+  const showhiddenMenu = () => {
     let show = document.querySelector(".menu-mobile--show");
     if (show) {
       mobile_menu.classList.remove("menu-mobile--show");
     } else {
       mobile_menu.classList.add("menu-mobile--show");
     }
+  };
+  //dar click para le mene resposive
+  mobile_btn.addEventListener("click", showhiddenMenu);
+
+  //redimension de pantalla
+  window.addEventListener("resize", () => {
+    let window_width = parseInt(document.body.clientWidth);
+
+    if (window_width >= 1000) {
+      mobile_menu.classList.remove("menu-mobile--show");
+    }
+  });
+
+  //? boton  x
+  let btn_close = document.querySelector(".menu-mobile__close");
+
+  btn_close.addEventListener("click", showhiddenMenu);
+
+  //? desplegar submenu
+
+  let menu_item = document.querySelectorAll(".menu-mobile__item");
+
+  menu_item.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      let submenu = item.lastElementChild;
+
+      if (submenu.className === ".menu-mobile__submenu-mobile") {
+        // console.log(submenu);
+        if ((submenu.style.display = "block")) {
+          submenu.style.display = "none";
+        } else {
+          submenu.style.display = "block";
+        }
+      }
+    });
   });
 });
