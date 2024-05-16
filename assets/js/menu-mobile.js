9ecba9c785c70a66a21faec038f019b5a671f3b4
william.quimbay <quimbayfeo@gmail.com>
2024-05-16 15:10:47 -0500

    final del proyecto

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     item.addEventListener("click", (event) => {
       let submenu = item.lastElementChild;
 
-      if (submenu.className === ".menu-mobile__submenu-mobile") {
+      if (submenu.className === "menu-mobile__submenu-mobile") {
         // console.log(submenu);
         if ((submenu.style.display = "block")) {
           submenu.style.display = "none";
